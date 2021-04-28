decf443e7bfd4718d7489a8283174023dbf99270
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-28 21:46:41 +0100

    create questions array

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const questions = [
+  {
+    title: "What is the Capitol of New Zealand?",
+    options: ["Wellington ", "Auckland", "Hamilton", "Tauranga"],
+    answer: "Wellington",
+  },
+  {
+    title: "What is the Capitol of Philippines?",
+    options: ["Quezon", "Manila", "Caloocan", "Davao"],
+    answer: "Manila",
+  },
+  {
+    title: "What is the Capitol of Japan?",
+    options: ["Yokohama", "Kyoto", "Tokyo", "Osaka"],
+    answer: "Tokyo",
+  },
+  {
+    title: "What is the Capitol of Thailand?",
+    options: ["Phuket", "Pattaya", "Chiang Mai", "Bangkok"],
+    answer: "Bangkok",
+  },
+  {
+    title: "What is the Capitol of Russia?",
+    options: ["Novosibirsk", "Krasnodar", "Moscow", "Kazan"],
+    answer: "Moscow",
+  },
+];
