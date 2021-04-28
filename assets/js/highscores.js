25c200ae604bba4e3c0f263d3640f3dd1a1799bd
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-28 21:55:15 +0100

    create highscore page on js

---
+++ assets/js/highscores.js
@@ -0,0 +1,35 @@
+const main = document.getElementById("high-scores-container");
+const clearButton = document.getElementById("clear-highscores");
+
+const renderHighScores = () => {
+  const highscores = getFromLocalStorage();
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "highscores");
+  ul.setAttribute("id", "highscores");
+
+  const renderListItem = (each) => {
+    const li = document.createElement("li");
+    li.textContent = `${each.user} - ${each.score}`;
+
+    ul.append(li);
+  };
+
+  highscores.forEach(renderListItem);
+
+  main.append(ul);
+};
+
+const clearHighscores = () => {
+  localStorage.clear();
+
+  const highscores = document.getElementById("highscores");
+
+  if (highscores) {
+    highscores.remove();
+  }
+};
+
+clearButton.addEventListener("click", clearHighscores);
+
+window.addEventListener("load", renderHighScores);
