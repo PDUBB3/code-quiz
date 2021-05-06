4549f379a7abfbd870130a4ceedbef3bca00e1a0
Pamela <pamela_bhopal@hotmail.co.uk>
2021-05-06 16:55:26 +0100

    amended font style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,26 @@
 const questions = [
   {
-    title: "What is the Capitol of New Zealand?",
+    title: "What is the Capital of New Zealand?",
     options: ["Wellington", "Auckland", "Hamilton", "Tauranga"],
     answer: "Wellington",
   },
   {
-    title: "What is the Capitol of Philippines?",
+    title: "What is the Capital of Philippines?",
     options: ["Quezon", "Manila", "Caloocan", "Davao"],
     answer: "Manila",
   },
   {
-    title: "What is the Capitol of Japan?",
+    title: "What is the Capital of Japan?",
     options: ["Yokohama", "Kyoto", "Tokyo", "Osaka"],
     answer: "Tokyo",
   },
   {
-    title: "What is the Capitol of Thailand?",
+    title: "What is the Capital of Thailand?",
     options: ["Phuket", "Pattaya", "Chiang Mai", "Bangkok"],
     answer: "Bangkok",
   },
   {
-    title: "What is the Capitol of Russia?",
+    title: "What is the Capital of Russia?",
     options: ["Novosibirsk", "Krasnodar", "Moscow", "Kazan"],
     answer: "Moscow",
   },
